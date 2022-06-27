a74b08026900418ce7e84f6f0cd4138052532c20
Nick Etuk <nick.etuk1@nhs.net>
2022-06-27 17:15:41 +0100

    Fixed branching

--- src/prototype/app/routes.js
+++ src/prototype/app/routes.js
@@ -1,8 +1,33 @@
-// External dependencies
 const express = require('express');
 
 const router = express.Router();
 
-// Add your routes here - above the module.exports line
+router.post('/version-a/branching/answer', (req, res) => {
+    // Make a variable and give it the value from 'know-nhs-number'
+    const nhsNumber = req.session.data['know-nhs-number'];
+  
+    // Check whether the variable matches a condition
+    if (nhsNumber === 'Yes') {
+      // Send user to next page
+      res.redirect('/version-a/branching/answer-yes');
+    } else {
+      // Send user to ineligible page
+      res.redirect('/version-a/branching/answer-no');
+    }
+  });
 
+router.post('/version-b/branching/answer', (req, res) => {
+    // Make a variable and give it the value from 'know-nhs-number'
+    const nhsNumber = req.session.data['know-nhs-number'];
+  
+    // Check whether the variable matches a condition
+    if (nhsNumber === 'Yes') {
+      // Send user to next page
+      res.redirect('/version-b/branching/answer-yes');
+    } else {
+      // Send user to ineligible page
+      res.redirect('/version-b/branching/answer-no');
+    }
+  });
+  
 module.exports = router;
